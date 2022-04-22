1ea17b9f13dfe40eb48bca0896078aebc22697f5
Justus <justus@justcoding.tech>
2022-04-22 15:07:53 +0200

    Use .env from current directory

--- downloader/index.js
+++ downloader/index.js
@@ -11,7 +11,7 @@ const legacy = require('legacy-encoding');
 
 (async () => {
     // Initialize .env file
-    require('dotenv').config();
+    require('dotenv').config( { path: `${__dirname}/.env` } );
     // Connect to dsbmobile "api"
     const dsb = new dsbmobile(process.env.DSBMOBILE_ID, process.env.DSBMOBILE_PASSWORD);
     // Get timetables from dsb
